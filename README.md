73ac370d613a92f29c85a772bbdc2ca1818ae401
CismonX <qiaocheng@twt.edu.cn>
2018-02-23 21:51:29 +0800

    add new files

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # boost-helper-json
-A simple wrapper around boost::property_tree::json_parser for quick &amp; dirty JSON access.
+A simple wrapper around Boost.PropertyTree for quick &amp; dirty JSON access.
+
+## 1. Introduction
+
+If you're working with JSON on C++ and looking for a good library, I'd recommend [RapidJSON](http://rapidjson.org/) or [JSON for Modern C++](https://nlohmann.github.io/json/). They are fast and scalable, and provides loads of useful functionalities.
+
+However, if you're just looking for a quick and dirty workaround, and happen to be using the Boost C++ libraries in your project, this may be an option for you.
+
+Just include the header, and start hacking with JSON.
+
+## 2. Example
+
+```c++
+#include "json.hpp"
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    using namespace boost_helper;
+
+    // Initialize JSON with a string.
+    json response("{\"err\":0,\"data\":{\"msg\":\"ok\",\"names\":[\"John\",\"Sarah\"]}}");
+
+    // Changing a node value with a string.
+    response["data"]["msg"] = "confirmed";
+
+    // Check whether this node has a child with the specified key.
+    if (response.has("err"))
+    {
+        // Changing a node value with another JSON node.
+        response["err"] = json("{\"code\":0,\"status\":\"ok\"}");
+    }
+
+    // Access child node value of a json.
+    std::cout << "Now message becomes \"" << response["data"]["msg"].val() << "\"" << std::endl;
+
+    // Iterate through a JSON node.
+    for (const auto& name : response["data"]["names"])
+    {
+        std::cout << name.second.val() << " said hello!" << std::endl;
+    }
+
+    // Iterate througth a JSON node and change its children's values.
+    for (auto name : response["data"]["names"])
+    {
+        name.second = "Nobody";
+    }
+
+    // Print a JSON node.
+    std::cout << response.to_string() << std::endl;
+}
+```
+
---
+++ json.hpp
@@ -0,0 +1,137 @@
+#pragma once
+
+#include <boost/property_tree/json_parser.hpp>
+
+namespace boost_helper
+{
+    /**
+     * A simple wrapper around Boost.PropertyTree for quick & dirty JSON access.
+     *
+     * @author CismonX
+     */
+    class json
+    {
+        using ptree = boost::property_tree::ptree;
+
+        /// The Boost.PropertyTree instance with stores this JSON structure.
+        ptree* data_;
+
+        /// Whether this is the node root of JSON object.
+        bool is_root_ = false;
+
+    public:
+
+        /// Trying to modify the value of root node of JSON.
+        class root_modification_exception : public std::exception {};
+
+        /// Range-based for support.
+        class iterator : public ptree::iterator
+        {
+        public:
+            explicit iterator(const ptree::iterator& other) : ptree::iterator(other) {}
+
+            std::pair<std::string, json> operator*() const
+            {
+                return make_pair(dereference().first, json(&dereference().second));
+            }
+        };
+
+        /**
+         * Iterator begin.
+         */
+        iterator begin() const
+        {
+            return iterator(data_->begin());
+        }
+
+        /**
+         * Iterator end.
+         */
+        iterator end() const
+        {
+            return iterator(data_->end());
+        }
+
+        /**
+         * Create a new JSON object with the given string.
+         */
+        explicit json(const std::string& json_str) : is_root_(true)
+        {
+            data_ = new boost::property_tree::ptree;
+            std::istringstream stream(json_str);
+            read_json(stream, *data_);
+        }
+
+        /**
+         * Create a JSON object with a ptree node.
+         */
+        explicit json(boost::property_tree::ptree* other) : data_(other) {}
+
+        /**
+         * Assign the value of this node with another node.
+         */
+        json& operator=(const json& other)
+        {
+            if (is_root_)
+                throw root_modification_exception();
+            *data_ = *other.data_;
+            return *this;
+        }
+
+        /**
+         * Assign the value of this node with a string.
+         */
+        json& operator=(const std::string& value)
+        {
+            if (is_root_)
+                throw root_modification_exception();
+            data_->put_value(value);
+            return *this;
+        }
+
+        /**
+         * Get child node.
+         */
+        json operator[](const std::string& key) const
+        {
+            if (data_->find(key) == data_->not_found())
+                data_->add_child(key, boost::property_tree::ptree());
+            return json(&data_->get_child(key));
+        }
+
+        /**
+         * Get string value of this node.
+         */
+        std::string val() const
+        {
+            return data_->get_value<std::string>();
+        }
+        
+        /**
+         * Check whether this node has a child with the specified key.
+         */
+        bool has(const std::string& key) const
+        {
+            return data_->find(key) != data_->not_found();
+        }
+
+        /**
+         * Print this JSON node to a string.
+         */
+        std::string to_string() const
+        {
+            std::ostringstream stream;
+            write_json(stream, *data_, false);
+            return stream.str();
+        }
+
+        /**
+         * Destructor.
+         */
+        ~json()
+        {
+            if (is_root_)
+                delete data_;
+        }
+    };
+}
